90fe76f3f950b9ce65698213966cd700e2fe248d
Zer02z2 <zongzechan@gmail.com>
2023-12-04 14:50:09 -0500

    not working

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
-import "./style.css";
-import p5 from 'p5';
-import 'p5/lib/addons/p5.sound'
-import * as Tone from 'tone'
+// import "./style.css";
+// import p5 from 'p5';
+// import 'p5/lib/addons/p5.sound'
+// import * as Tone from 'tone'
 
-const sketch = (p) => {
-  p.setup = () => {
-    p.createCanvas(400, 400);
-  };
+// const sketch = (p) => {
+//   p.setup = () => {
+//     p.createCanvas(400, 400);
+//   };
 
-  p.draw = () => {
-    p.background(220);
-    // Your p5.js drawing code here
-  };
-};
+//   p.draw = () => {
+//     p.background(220);
+//     // Your p5.js drawing code here
+//   };
+// };
 
-new p5(sketch);
+// new p5(sketch);
+
+function setup() {
+  
+  createCanvas(400, 400);
+  background(0);
+
+  // let tone = new Tone.Synth().toDestination();
+
+}
+
+function draw() {
+
+}
